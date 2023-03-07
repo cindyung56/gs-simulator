2a002b7c2ba8b439b30db10706f1236b4f37068a
cindy <cindyung56@gmail.com>
2023-03-06 16:35:41 -0800

    styled buttons

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,16 +6,29 @@ export default function Home(props) {
 
   return (
     <div className="main-interface">
-      <h1>HOME PAGE</h1>
-      <button className="details-btn" onClick={() => updateView("details")}>Details</button>
-      <button className="inventory-btn" onClick={() => updateView("inventory")}></button>
-      
+      <div className="info-buttons-container">
+        <button className="details-btn" onClick={() => updateView("details")}>
+          i
+        </button>
+        <button
+          className="inventory-btn"
+          onClick={() => updateView("inventory")}
+        >
+          <div className="small-square"></div>
+          <div className="small-square"></div>
+          <div className="small-square"></div>
+          <div className="small-square"></div>
+        </button>
+      </div>
+
       <div className="pull-buttons-container">
         <button className="pull-btn" onClick={() => updateView("wish")}>
           Pull 1x
+          <p className="currency-cost">250</p>
         </button>
         <button className="pull-btn" onClick={() => updateView("wish")}>
           Pull 10x
+          <p className="currency-cost">2500</p>
         </button>
       </div>
     </div>
