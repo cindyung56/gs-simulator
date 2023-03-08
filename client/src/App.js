645dfc341dcb5efe44196d708237034a97def013
cindy <cindyung56@gmail.com>
2023-03-07 17:02:47 -0800

    added banners and active state

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,22 @@ import Inventory from "./components/Inventory";
 import Wish from "./components/Wish";
 
 function App() {
+  const bannerList = [
+    {
+      id: 1,
+      bannerArt:
+        "https://cdn.wikiwiki.jp/to/w/sidem-gstars/%E3%80%90%E3%82%AC%E3%82%B7%E3%83%A3%E3%80%91%E3%80%8C%E4%BC%9D%E3%81%88%E3%81%9F%E3%81%84%E6%83%B3%E3%81%84%E3%82%92%E9%81%8B%E3%82%93%E3%81%A7%20Teddy%20bear%EF%BC%86Letter%E3%82%AC%E3%82%B7%E3%83%A3%E3%80%8D/::ref/banner.png.webp?rev=82626f6864527620623af2c16697c3a5&t=20230228163829",
+    },
+    {
+      id: 2,
+      bannerArt:
+        "https://cdn.wikiwiki.jp/to/w/sidem-gstars/%E3%80%90%E3%82%AC%E3%82%B7%E3%83%A3%E3%80%91%E3%80%8C%E3%83%88%E3%83%AA%E3%83%83%E3%82%AF%E3%82%92%E6%B1%BA%E3%82%81%E3%82%8D%EF%BC%81Street%20Skateboard%E3%82%AC%E3%82%B7%E3%83%A3%E3%80%8D/::ref/banner.png.webp?rev=d0017b06060ef57564b2c1bdb09af2ae&t=20230217160205",
+    },
+  ];
+
   const [view, setView] = useState("home");
   // const [bannerData, setBannerData] = useState(null);
+  const [activeBanner, setActiveBanner] = useState(bannerList[0].id);
 
   // update view to go to the home page
   function goHome() {
@@ -22,6 +36,10 @@ function App() {
     setView(v);
   }
 
+  function changeActiveBanner(b) {
+    setActiveBanner(b.id);
+  }
+
   function updateBrowser() {
     switch (view) {
       case "details":
@@ -31,7 +49,7 @@ function App() {
       case "wish":
         return <Wish goHome={goHome}/>;
       default:
-        return <Home updateView={updateView}/>;
+        return <Home bannerList={bannerList} activeBanner={activeBanner} updateView={updateView} changeActiveBanner={changeActiveBanner} />;
     }
   }
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,7 +2,26 @@ import React, { useState, useEffect } from "react";
 import "./style.css";
 
 export default function Home(props) {
-  const { updateView } = props;
+  // const bannerList = [
+  //   {
+  //     id: 1,
+  //     bannerArt:
+  //       "https://cdn.wikiwiki.jp/to/w/sidem-gstars/%E3%80%90%E3%82%AC%E3%82%B7%E3%83%A3%E3%80%91%E3%80%8C%E4%BC%9D%E3%81%88%E3%81%9F%E3%81%84%E6%83%B3%E3%81%84%E3%82%92%E9%81%8B%E3%82%93%E3%81%A7%20Teddy%20bear%EF%BC%86Letter%E3%82%AC%E3%82%B7%E3%83%A3%E3%80%8D/::ref/banner.png.webp?rev=82626f6864527620623af2c16697c3a5&t=20230228163829",
+  //   },
+  //   {
+  //     id: 2,
+  //     bannerArt:
+  //       "https://cdn.wikiwiki.jp/to/w/sidem-gstars/%E3%80%90%E3%82%AC%E3%82%B7%E3%83%A3%E3%80%91%E3%80%8C%E3%83%88%E3%83%AA%E3%83%83%E3%82%AF%E3%82%92%E6%B1%BA%E3%82%81%E3%82%8D%EF%BC%81Street%20Skateboard%E3%82%AC%E3%82%B7%E3%83%A3%E3%80%8D/::ref/banner.png.webp?rev=d0017b06060ef57564b2c1bdb09af2ae&t=20230217160205",
+  //   },
+  // ];
+
+  const { bannerList, activeBanner, updateView, changeActiveBanner } = props;
+
+  // const [activeBanner, setActiveBanner] = useState(bannerList[0].id);
+
+  // function changeActiveBanner(b) {
+  //   setActiveBanner(b.id);
+  // }
 
   return (
     <div className="main-interface">
@@ -30,6 +49,32 @@ export default function Home(props) {
           Pull 10x
           <p className="currency-cost">2500</p>
         </button>
+      </div>
+
+      <div className="banner-carousel">
+        {/* map each banner in carousel */}
+        {bannerList.map((b) => (
+          <div
+            key={b.id}
+            className={b.id === activeBanner ? "active-banner" : ""}
+            style={{
+              position: "relative",
+              width: "100%",
+              marginTop: "1vh",
+            }}
+            onClick={() => changeActiveBanner(b)}
+          >
+            <img
+              src={b.bannerArt}
+              alt="banner"
+              style={{
+                width: "100%",
+                height: "100%",
+                cursor: "pointer",
+              }}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
