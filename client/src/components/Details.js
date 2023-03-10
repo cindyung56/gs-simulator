0b35b8a4dfcd42cbced85be808db453dd9d76f12
cindy <cindyung56@gmail.com>
2023-03-10 14:21:14 -0800

    added effects to change bg image

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -1,13 +1,13 @@
 import React from "react";
-import './style.css';
+import "./style.css";
 
-export default function Details(props){
-    const {goHome} = props;
+export default function Details(props) {
+  const { goHome } = props;
 
-    return(
-        <div>
-        <h1>DETAILS PAGE</h1>
-        <button onClick={goHome}>Back</button>
+  return (
+    <div className="details-container">
+      <h1>DETAILS PAGE</h1>
+      <button onClick={goHome}>Back</button>
     </div>
-    )
-};
+  );
+}
