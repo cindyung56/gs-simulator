f817018e1b6f47b87121ea8ba67ce9468ba88c8e
cindy <cindyung56@gmail.com>
2023-03-13 17:27:09 -0700

    added content in details component

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 
 export default function Details(props) {
   const { goHome } = props;
 
+  // const [activeOption, setActiveOption] = useState("");
+
   return (
     <div className="details-container">
-      <h1>DETAILS PAGE</h1>
-      <button onClick={goHome}>Back</button>
+      {/* <h1>DETAILS PAGE</h1> */}
+      <div className="details-options">
+      <div className="d-option">Information</div>
+      <div className="d-option">Probabilities</div>
+      <div className="d-option">Update History</div>
+      </div>
+      <div className="details"></div>
+      <button className="go-home-btn" onClick={goHome}>←</button>
     </div>
   );
 }
