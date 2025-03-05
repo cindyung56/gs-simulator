c584cadb599a5ff07bc220b57d9051097000edd9
cindy <cindyung56@gmail.com>
2025-03-05 14:35:41 -0800

    added functions to generate card rarity

--- client/src/components/Wish.js
+++ client/src/components/Wish.js
@@ -4,21 +4,52 @@ import "./style.css";
 export default function Results(props) {
   const { goHome, pull } = props;
 
-  function pullOne() {
-    const r = Math.floor(Math.random() * 100);
-    return r;
+  function convertToCardResult(result) {
+    if (result < 4) return "SRR";
+    else if (result < 20) return "SR";
+    else return "R";
   }
 
+  // pull one result with normal rates
+  function pullOne() {
+    const r = Math.floor(Math.random() * 100);
+    return convertToCardResult(r);
+  }
+
+  // function to generate 10 random results
+  // pull 9 cards first and check if there was at least 1 SR or above, change SRAbove to be true if so
+  // for the 10th card, we check SRAbove; 
+  // if true, pull one more card with normal rates; 
+  // if false, pull from 0 to 20 to get guaranteed SR or above
+  // finally, return the array
   function pullTen() {
     const rArray = [];
-    for (let i = 0; i < 10; i++) {
+    let SRAbove = false;
+    for (let i = 0; i < 9; i++) {
       const r = Math.floor(Math.random() * 100);
-      rArray.push(r);
+      if (r < 10) {
+        SRAbove = true;
+      }
+      rArray.push(convertToCardResult(r));
     }
+    SRAbove
+      ? rArray.push(pullOne())
+      : rArray.push(convertToCardResult(Math.floor(Math.random() * 20)));
     return rArray;
   }
 
-  const pulls = pull === 1 ? pullOne() : pullTen();
+  // function to get card pictures/data from database based on card number and rarity
+  function generateCards() {}
+
+  function displayCards(rArray) {
+    return rArray.map((r, i) => (
+      <div key={i} className="card">
+        <p>{r}</p>
+      </div>
+    ));
+  }
+
+  const pulls = pull === 1 ? pullOne() : displayCards(pullTen());
 
   return (
     <div>
